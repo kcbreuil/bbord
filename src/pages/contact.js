9a68c6f62171c1ae202bb4af12fc3acb32c57a42
kcbreuil <kcbreuil@gmail.com>
2021-03-22 15:45:35 -0400

    form updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,28 +2,31 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "styled-components"
-import Draggable from 'react-draggable';
 import Img from 'gatsby-image'
 
 import logo from '../images/logo.png'
 import ContactForm from '../components/contactForm'
 
-const ContactStyle = styled.div`
+const ContactStyle = styled.img`
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  height: 250px;
+  margin-top: 150px;
+  margin-left: 50px;
 `
-const StyledLabel = styled.label`
-  display: flex;
-`
+
+const copy = {
+  heading: 'let\s connect',
+  text: 'submit'
+
+}
 
 export default function ContactPage() {
   return (
     <Layout>
       <SEO title="Contact" />
-      <h1>let's connect</h1>
-          <Img fluid={logo} alt="logo" />
-      
-      <ContactForm />
+      <ContactStyle src={logo} alt="logo" />
+      <ContactForm formHeading={copy.heading} buttonText={copy.text}/>
     </Layout>
   )
   
